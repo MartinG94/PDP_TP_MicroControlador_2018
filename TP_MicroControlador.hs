a2bc50fe36d1235be7baa46a20fd6db9fb371e85
Unknown <guillenmartin94@gmail.com>
2018-05-09 09:11:12 -0300

    Eliminando nops
    _ Se eliminó la función nop de todas las instrucciones.
    _ nop debe ejecutarse cada vez que se implemente una Instrucción de un Programa.

--- TP_MicroControlador.hs
+++ TP_MicroControlador.hs
@@ -30,26 +30,26 @@ nop :: Instrucción
 nop micro = nuevoProgramCounter (programCounter micro + 1) micro
 
 lodv :: Int -> Instrucción
-lodv unValor micro = (nop . nuevoAcum_A unValor) micro
+lodv unValor micro = nuevoAcum_A unValor micro
 
 swap :: Instrucción
-swap micro = (nop . nuevoAcum_A (acumulador_B micro) . nuevoAcum_B (acumulador_A micro)) micro
+swap micro = (nuevoAcum_A (acumulador_B micro) . nuevoAcum_B (acumulador_A micro)) micro
 
 add :: Instrucción
-add micro = (nop . nuevoAcum_B 0 . nuevoAcum_A (acumulador_A micro + acumulador_B micro)) micro
+add micro = (nuevoAcum_B 0 . nuevoAcum_A (acumulador_A micro + acumulador_B micro)) micro
 
 type Posición = Int
 
 str :: Posición -> Int -> Instrucción
-str unaPosición unValor micro = (nop . nuevaMemoria ((take (unaPosición - 1) (memoria micro)) ++ [unValor] ++ (drop unaPosición (memoria micro)))) micro
+str unaPosición unValor micro = (nuevaMemoria ((take (unaPosición - 1) (memoria micro)) ++ [unValor] ++ (drop unaPosición (memoria micro)))) micro
 
 lod :: Posición -> Instrucción
-lod unaPosición micro = (nop . nuevoAcum_A ((!!) (memoria micro) (unaPosición - 1))) micro
+lod unaPosición micro = (nuevoAcum_A ((!!) (memoria micro) (unaPosición - 1))) micro
 
 divide :: Instrucción
 divide micro
-  | (not . (==0) . acumulador_B) micro = (nop . nuevoAcum_B 0 . nuevoAcum_A (acumulador_A micro `div` acumulador_B micro)) micro
-  | otherwise = (nop . nuevaEtiqueta "DIVISION BY ZERO") micro
+  | (not . (==0) . acumulador_B) micro = (nuevoAcum_B 0 . nuevoAcum_A (acumulador_A micro `div` acumulador_B micro)) micro
+  | otherwise = (nuevaEtiqueta "DIVISION BY ZERO") micro
 
 fp20 = MicroControlador [] 7 24 0 "" []
 at8086 = MicroControlador [1.. 20] 0 0 0 "" []
