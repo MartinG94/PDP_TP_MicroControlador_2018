56fedd390bfd987fd8266e86d3741c54c43e9065
MartinG94 <37748273+MartinG94@users.noreply.github.com>
2018-05-08 23:12:11 -0300

    Nueva variable en MicroControlador
    _ Ahora, cada MicroControlador tiene una lista de programas asociado
    _ Nueva función auxiliar que agrega un programa a la lista de programas de un MicroControlador

--- TP_MicroControlador.hs
+++ TP_MicroControlador.hs
@@ -4,21 +4,24 @@ import Data.List
 import Data.Maybe
 import Test.Hspec
 
+-- 1ra Parte
+
 data MicroControlador = MicroControlador {
   memoria :: [Int],
   acumulador_A :: Int,
   acumulador_B :: Int,
   programCounter :: Int,
-  mensajeError :: String
-} deriving(Show,Eq)
+  mensajeError :: String,
+  programas :: [Programa]
+} deriving(Show)
 
-nuevaMemoria otraMemoria microControlador = microControlador {memoria = otraMemoria}
-nuevoAcum_A otroAcum_A microControlador = microControlador {acumulador_A = otroAcum_A}
-nuevoAcum_B otroAcum_B microControlador = microControlador {acumulador_B = otroAcum_B}
-nuevoProgramCounter otroPC microControlador = microControlador {programCounter = otroPC}
-nuevaEtiqueta otraEtiqueta microControlador = microControlador {mensajeError = otraEtiqueta}
+nuevaMemoria otraMemoria micro = micro {memoria = otraMemoria}
+nuevoAcum_A otroAcum_A micro = micro {acumulador_A = otroAcum_A}
+nuevoAcum_B otroAcum_B micro = micro {acumulador_B = otroAcum_B}
+nuevoProgramCounter otroPC micro = micro {programCounter = otroPC}
+nuevaEtiqueta otraEtiqueta micro = micro {mensajeError = otraEtiqueta}
 
-xt8088 = MicroControlador [] 0 0 0 ""
+xt8088 = MicroControlador [] 0 0 0 "" []
 
 type Instrucción = MicroControlador -> MicroControlador
 type Programa = MicroControlador -> MicroControlador
@@ -48,8 +51,8 @@ divide micro
   | (not . (==0) . acumulador_B) micro = (nop . nuevoAcum_B 0 . nuevoAcum_A (acumulador_A micro `div` acumulador_B micro)) micro
   | otherwise = (nop . nuevaEtiqueta "DIVISION BY ZERO") micro
 
-fp20 = MicroControlador [] 7 24 0 ""
-at8086 = MicroControlador [1.. 20] 0 0 0 ""
+fp20 = MicroControlador [] 7 24 0 "" []
+at8086 = MicroControlador [1.. 20] 0 0 0 "" []
 
 avanzarTresPosiciones :: Programa
 avanzarTresPosiciones = nop.nop.nop
@@ -57,8 +60,14 @@ avanzarTresPosiciones = nop.nop.nop
 sumar10Y22 :: Programa
 sumar10Y22 = add . lodv 22 . swap . lodv 10
 
-intentarDividir2Por0 :: Programa
-intentarDividir2Por0 = divide . lod 1 . swap . lod 2 . str 2 0 . str 1 2
+diviciónDe2Por0 :: Programa
+diviciónDe2Por0 = divide . lod 1 . swap . lod 2 . str 2 0 . str 1 2
 
-dividir12Por4 :: Programa
-dividir12Por4 = divide . lod 1 . swap . lod 2 . str 2 4 . str 1 12
+diviciónDe12Por4 :: Programa
+diviciónDe12Por4 = divide . lod 1 . swap . lod 2 . str 2 4 . str 1 12
+
+-- 2da Parte
+
+agregarPrograma unPrograma micro = micro {programas = programas micro ++ [unPrograma]}
+
+cargar unPrograma = agregarPrograma unPrograma
