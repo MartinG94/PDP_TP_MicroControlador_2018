4b20176929aa191b1a02d08b1f63918361fc35cf
MartinG94 <37748273+MartinG94@users.noreply.github.com>
2018-05-08 21:57:02 -0300

    Expresividad y Tests
    _ Eliminación de los tests
    _ Programas más expresivos

--- TP_MicroControlador.hs
+++ TP_MicroControlador.hs
@@ -50,26 +50,7 @@ divide micro
 fp20 = MicroControlador [] 7 24 0 ""
 at8086 = MicroControlador [1.. 20] 0 0 0 ""
 
-programa1 = nop.nop.nop
-programa2 = add . lodv 22 . swap . lodv 10
-programa3 = divide . lod 1 . swap . lod 2 . str 2 0 . str 1 2
-programa3b = divide . lod 1 . swap . lod 2 . str 2 4 . str 1 12
-
-testsPrimeraEntrega = hspec $ do
-  describe "Probando las instrucciones." $ do
-    it "Se modela un programa que haga avanzar tres posiciones el program counter." $
-      (programCounter . programa1) xt8088 `shouldBe` 3
-    it "Se modela un programa que permite sumar 10 + 22." $
-      (acumulador_A . programa2) xt8088 `shouldBe` 32
-    it "Se modela un programa que intenta dividir 2 por 0." $
-      (mensajeError . programa3) xt8088 `shouldBe` "DIVISION BY ZERO"
-    it "Dado un procesador fp20 que tiene acumulador A con 7 y acumulador B con 24, al ejecutar SWAP el acumulador A debe quedar con 24 y el B con 7." $
-      swap fp20 `shouldBe` MicroControlador [] 24 7 1 ""
-    it "Ejecutamos la instrucción STR 2 5 en at8086. Entonces el procesador at8086 debe quedar con un 5 en la posición 2: [1, 5, 3, 4, 5,... ]" $
-      (flip (!!) 1 . memoria . str 2 5) at8086 `shouldBe` 5
-    it "LOD 2 de un procesador xt8088 con la memoria vacía (1024 posiciones con valores cero) debe dejar con cero el acumulador A (cero = ausencia de información)" $
-      (acumulador_A . lod 2 . nuevaMemoria [0,0..]) xt8088  `shouldBe` 0
-    it "Ejecutar por consola la división 2 por 0 para el procesador xt8088 según el programa escrito arriba, esperamos el mensaje de error “DIVISION BY ZERO”, y un 6 en el program counter." $
-      programa3 xt8088 `shouldBe` MicroControlador [2,0] 2 0 6 "DIVISION BY ZERO"
-    it "Ejecutar la división de 12 por 4 para el procesador xt8088 (cambiando los valores del programa anterior), que debe dar 3 y no tirar ningún mensaje de error" $
-      programa3b xt8088 `shouldBe` MicroControlador [12,4] 3 0 6 ""
+avanzarTresPosiciones = nop.nop.nop
+sumar10Y22 = add . lodv 22 . swap . lodv 10
+intentarDividir2por0 = divide . lod 1 . swap . lod 2 . str 2 0 . str 1 2
+dividir12Por4 = divide . lod 1 . swap . lod 2 . str 2 4 . str 1 12
